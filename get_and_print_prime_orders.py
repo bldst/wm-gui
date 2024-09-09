2db8de9018096013cc148d93e4329df97d895e0a
cc <1677931886@qq.com>
2024-09-09 23:18:03 +0800

    重构在线订单查询并添加本地查询选项
    重构代码以先判断是否搜索在线订单。若否，则通过新函数`get_prime_prices_local`从CSV文件中获取数据；若是，则保持原有在线查询逻辑不变。此次改动还简化了错误处理，移除了未使用的导入。

--- get_and_print_prime_orders.py
+++ get_and_print_prime_orders.py
@@ -1,3 +1,4 @@
+import csv
 import logging
 import requests
 
@@ -18,6 +19,7 @@ class PrimeOrdersProcess:
     传入url_name列表，
     返回一个字典，
     key为url_name，value为在线订单前三个价格列表
+    {'galatine_prime_handle': '['galatine_prime_handle', '3', '4', '5']'}
     """
 
     def __init__(self, prime_url_name_list, search_online=True):
@@ -31,16 +33,22 @@ class PrimeOrdersProcess:
             print(f"{key}: {value}")
 
     def main(self, prime_url_name_list):
-        # 是否查询在线订单
-        if self.search_online:
+
             for url_name in prime_url_name_list:
-                #得到该url_name的订单数据
-                order_json = self.fetch_sell_orders(url_name)
-                # 返回带有3个价格的订单列表
-                price_list = self.extract_and_filter_orders(order_json)
+                # 是否查询在线订单
+                if self.search_online:
+                    #得到该url_name的订单数据
+                    order_json = self.fetch_sell_orders(url_name)
+                    # 返回带有3个价格的订单列表
+                    price_list = self.extract_and_filter_orders(order_json)
+                else:
+                    # 本地查询
+                    price_list = self.get_prime_prices_local(url_name)
                 # 构造指定格式列表[url_name,price_list[0],price_list[1],price_list[2]]，添加到字典
                 self.prime_dict[url_name] = [url_name] + price_list
-        # 本地查询
+
+
+
     def fetch_sell_orders(self, url_name):
         url = f'https://api.warframe.market/v1/items/{url_name}/orders'
         try:
@@ -68,7 +76,21 @@ class PrimeOrdersProcess:
 
         return top_prices
 
+    # 本地查询
+    def get_prime_prices_local(self,url_name):
+        top_prices=[]
+        # 从本地读取primeprice.csv文件中的订单数据
+        with open('static/primeprice.csv', mode='r', encoding='utf-8') as file:
+            reader = csv.DictReader(file)  # 创建一个字典阅读器
+            for row in reader:  # 遍历每一行
+                if row['url_name'] == url_name:
+                    top_prices.append(row['platinum1'])
+                    top_prices.append(row['platinum2'])
+                    top_prices.append(row['platinum3'])
+                    break  # 找到后停止搜索
+        return top_prices
+
 
 if __name__ == "__main__":
-    prime_url_name_list = ['helios_prime_systems', 'mirage_prime_systems_blueprint','zylok','galatine_prime_handle','hystrix_prime_set','khora_prime_set']
-    orders = PrimeOrdersProcess(prime_url_name_list)
+    prime_url_name_list = ['galatine_prime_handle','hystrix_prime_set','khora_prime_set']
+    orders = PrimeOrdersProcess(prime_url_name_list,0)
